e02f59da53d0443fb6d83eae5efdd24d3cdd95ff
Gabriel27109812362 <gabriel.opolski98362@gmail.com>
2021-03-03 12:41:37 +0100

    finish all app styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
+import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'signUp', component: SignUpComponent }];
+const routes: Routes = [
+  { path: 'signUp', component: SignUpComponent },
+  { path: 'signIn', component: SignInComponent },
+  { path: '', redirectTo: '/signIn', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
